4cad88fdc1cef128057942c11b7b5f3af9d3b41f
Allan Scala <allanscala@gmail.com>
2022-09-22 14:23:32 -0300

    feat: create user service

--- src/main/java/com/example/videoplace/api/repositories/UserRepository.java
+++ src/main/java/com/example/videoplace/api/repositories/UserRepository.java
@@ -2,8 +2,10 @@ package com.example.videoplace.api.repositories;
 
 import com.example.videoplace.api.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
 }
---
+++ src/main/java/com/example/videoplace/api/services/UserService.java
@@ -0,0 +1,19 @@
+package com.example.videoplace.api.services;
+
+import com.example.videoplace.api.models.UserModel;
+import com.example.videoplace.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Transactional
+    public UserModel save(UserModel userModel) {
+        return userRepository.save(userModel);
+    }
+}
